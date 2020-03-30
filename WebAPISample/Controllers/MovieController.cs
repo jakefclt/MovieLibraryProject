f5ff12540115987eb9fe33ddb2b73ef9b8d9e32d
Matthew VH <vanheckematt@gmail.com>
2020-03-30 16:11:50 -0500

    Post method is now operational.

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -42,6 +42,8 @@ namespace WebAPISample.Controllers
         public IActionResult Post([FromBody]Movie value)
         {
             // Create movie in db logic
+            _context.Movies.Add(value);
+            _context.SaveChanges();
             return Ok();
         }
 
