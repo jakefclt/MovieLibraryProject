8a1774de2b220a56719103b4f7beaba4e76310e4
unknown <jake11b4v@gmail.com>
2020-03-31 12:02:35 -0500

    Queried database for list of movie objects to display on homepage. Wrote function to append rows to movie table through DOM manipulation

---
+++ app.js
@@ -0,0 +1,27 @@
+"use strict";
+
+$(document).ready(function(){
+
+    // queried database for list of movie object
+    $.ajax({
+        type: "GET", 
+        url: "https://localhost:44325/api/movie",
+        contentType: "application/json ;charset = utf-8",    
+        dataType: "json",
+
+        // manipulated DOM to append rows to movie table
+        success: function(data){
+            $("#movie-list").html(
+            '');
+           $.each(data, function (i, item){
+               var movie = "<tr>" +
+               "<td>" + item.title + "</td>" +
+               "<td>" + item.director + "</td>" +
+               "<td>" + item.genre + "</td>" +
+               "</tr>";
+
+               $('#movie-list').append(movie);
+           })
+        }
+    });
+});
